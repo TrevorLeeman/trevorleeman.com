291ba3f08e7d0408260e32b8e4fef902deb9b5c9
Trevor Leeman <tlemurz@gmail.com>
2022-05-09 01:20:03 -0700

    Reduce unused code

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,16 +3,14 @@ import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import MainContent from "../mainContent/MainContent";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div
-      className={`flex min-h-screen flex-col text-theme-black dark:bg-theme-black dark:text-theme-white`}
-    >
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <div className="flex flex-col text-theme-black transition duration-200 dark:bg-theme-black dark:text-theme-white">
+    <div className="flex min-h-screen flex-col">
       <Header />
       <MainContent>{children}</MainContent>
-      <Footer />
     </div>
-  );
-};
+    <Footer />
+  </div>
+);
 
 export default Layout;

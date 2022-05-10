d3f1b455bee52455bc584a13a1a09b69d5395064
Trevor Leeman <tlemurz@gmail.com>
2022-05-10 01:24:30 -0700

    Add SEO meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,30 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
+  const DESCRIPTION =
+    "Trevor Leeman is a software engineer based out of Seattle, WA. His primary focus is on Frontend Engineering using React, NextJS, TypeScript, and CSS.";
+  const OG_IMAGE_URL = "https://imgur.com/a/flvPVwA";
+
   return (
     <Html lang="en">
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Trevor Leeman" />
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={OG_IMAGE_URL} />
+        <meta property="og:image:url" content={OG_IMAGE_URL} />
+        <meta property="twitter:image" content={OG_IMAGE_URL} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Abel&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,30 @@ import Head from "next/head";
 import Socials from "../components/socials/Socials";
 
 const Homepage = () => {
+  const CANONICAL_URL = "https://www.trevorleeman.com";
+
   return (
     <>
       <Head>
         <title>Trevor Leeman</title>
+        <meta property="og:title" content="Trevor Leeman" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={CANONICAL_URL} />
+        <link rel="canonical" href={CANONICAL_URL} />
       </Head>
       <div className="flex flex-grow flex-col items-center justify-center pb-52">
-        <div className="flex flex-col pb-7">
-          <h1 className="text-xl font-semibold md:text-3xl">Hi, my name is</h1>
-          <h1>
-            <span className="text-7xl font-bold text-theme-purple dark:text-theme-pink md:text-9xl">
+        <h1 className="flex flex-col pb-7 font-abel">
+          <span className="text-2xl font-semibold md:text-4xl">
+            Hi, my name is
+          </span>
+          <span>
+            <span className="font-default text-8xl font-bold text-theme-purple dark:text-theme-pink md:text-[9.5rem]">
               Trevor
             </span>
-            <span className="text-md font-semibold md:text-xl"> and</span>
-          </h1>
-          <h1 className="text-2xl md:text-4xl">I'M A FRONTEND ENGINEER</h1>
-        </div>
+            <span className="text-lg font-semibold md:text-2xl"> and</span>
+          </span>
+          <span className="text-3xl md:text-5xl">I'M A FRONTEND ENGINEER</span>
+        </h1>
         <Socials />
       </div>
     </>

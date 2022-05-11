238783523ec2775d0372014c5407a7c762ef6a2f
Trevor Leeman <tlemurz@gmail.com>
2022-05-11 01:10:40 -0700

    Update og meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   const DESCRIPTION =
-    "Trevor Leeman is a software engineer based out of Seattle, WA. His primary focus is on Frontend Engineering using React, NextJS, TypeScript, and CSS.";
-  const OG_IMAGE_URL = "https://imgur.com/a/flvPVwA";
+    'Trevor Leeman is a software engineer based out of Seattle, WA. His primary focus is on Frontend Engineering using React, NextJS, TypeScript, and CSS.';
+  const OG_IMAGE_URL = 'https://imgur.com/a/flvPVwA';
 
   return (
     <Html lang="en">
@@ -12,19 +12,14 @@ const Document = () => {
         <meta name="description" content={DESCRIPTION} />
         <meta property="og:description" content={DESCRIPTION} />
         <meta property="og:image" content={OG_IMAGE_URL} />
-        <meta property="og:image:url" content={OG_IMAGE_URL} />
+        <meta property="og:image:secure_url" content={OG_IMAGE_URL} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="twitter:image" content={OG_IMAGE_URL} />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Abel&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />

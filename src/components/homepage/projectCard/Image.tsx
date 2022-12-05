aa439353cf48889c73b273e610649af3d4935d34
Trevor Leeman <Tlemurz@gmail.com>
2022-12-04 18:58:02 -0800

    Codemod to use legacy image tag

--- src/components/homepage/projectCard/Image.tsx
+++ src/components/homepage/projectCard/Image.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const ProjectCardImage = ({ src, alt }: { src: string; alt: string }) => (
   <div className="relative h-[200px] w-full sm:h-[300px] lg:h-[350px]">
--- src/components/homepage/projectCard/TechIcon.tsx
+++ src/components/homepage/projectCard/TechIcon.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 
 const ProjectCardTechIcon = ({ src, title }: { src: string; title: string }) => (
   <div className="relative h-8 w-8 sm:h-9 sm:w-9 md:h-10 md:w-10">

2cbf3d129c28be92a207e2af505624d912476f3c
Trevor Leeman <Tlemurz@gmail.com>
2022-12-04 21:00:06 -0800

    Update OSRS Exchange card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,25 +44,27 @@ const Homepage = () => {
         </h2>
         <div className="grid w-full gap-3 4xs:gap-5 3xs:gap-6 lg:grid-cols-2 xl:max-w-screen-lg xl:px-0">
           <ProjectCard>
-            <ProjectCardImage src="https://i.imgur.com/4pWNat5.png" alt="Coming Soon" />
+            <ProjectCardImage src="https://i.imgur.com/cC2ggwV.png" alt="OSRS Exchange site preview" />
             <ProjectCardInfo
               purpose="MMORPG Trading Tool"
-              title="OSRS Prices (WIP)"
-              description="Item flipping utility for the game Old School Runescape. Leverages the OSRS wiki Real-Time Prices API to suggest the most profitable items to flip in real time."
+              title="OSRS Exchange"
+              description="Market analysis utility for the game Old School Runescape. Leverages the OSRS wiki Real-Time Prices API to suggest the most profitable items to flip in real time."
+              liveLink="https://www.osrs.exchange"
               codeLink="https://github.com/TrevorLeeman/osrs-prices"
               techIcons={[
                 <ProjectCardTechIcon src="/icons/typescript.svg" title="TypeScript" key="TypeScript" />,
                 <ProjectCardTechIcon src="/icons/react.svg" title="React" key="React" />,
                 <ProjectCardTechIcon src="/icons/nextjs.svg" title="NextJS" key="NextJS" />,
-                <ProjectCardTechIcon src="/icons/scss.svg" title="SCSS" key="SCSS" />,
+                <ProjectCardTechIcon src="/icons/tailwind.svg" title="Tailwind CSS" key="Tailwind CSS" />,
                 <ProjectCardTechIcon src="/icons/postgres.svg" title="Postgres" key="Postgres" />,
                 <ProjectCardTechIcon src="/icons/docker.svg" title="Docker" key="Docker" />,
                 <ProjectCardTechIcon src="/icons/vercel.svg" title="Vercel" key="Vercel" />,
+                <ProjectCardTechIcon src="/icons/railway.svg" title="Railway" key="Railway" />,
               ]}
             />
           </ProjectCard>
           <ProjectCard>
-            <ProjectCardImage src="https://i.imgur.com/FwHcrj6.png" alt="Tic Tac Toe" />
+            <ProjectCardImage src="https://i.imgur.com/FwHcrj6.png" alt="Tic Tac Toe site preview" />
             <ProjectCardInfo
               purpose="Progressive Web App"
               title="Tic Tac Toe"
